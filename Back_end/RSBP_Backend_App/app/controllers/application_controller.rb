e53334d1ed560edaca52008d9f5ee20d7c038dbd
jangelroa <jangelroa@gmail.com>
2014-12-15 16:29:30 -0800

    _In app/controllers/application_controller.rb added:         protect_from_forgery with: :exception,         if: Proc.new { |c| c.request.format =~ %r{application/json} }     _Changed the routes to:           resources :event_news, except: [:new, :edit]           resources :attendances, except: [:new, :edit]           resources :users, except: [:new, :edit] do             resources :events, except: [:new, :edit]           end

--- Back_end/RSBP_Backend_App/app/controllers/application_controller.rb
+++ Back_end/RSBP_Backend_App/app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
 
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+
+  protect_from_forgery with: :exception,
+  if: Proc.new { |c| c.request.format =~ %r{application/json} }
 end
--- Back_end/RSBP_Backend_App/config/routes.rb
+++ Back_end/RSBP_Backend_App/config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  resources :event_news
-
-  resources :attendances
-
-  resources :events
-
-  resources :users
+  resources :event_news, except: [:new, :edit]
+  resources :attendances, except: [:new, :edit]
+  resources :users, except: [:new, :edit] do
+    resources :events, except: [:new, :edit]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
